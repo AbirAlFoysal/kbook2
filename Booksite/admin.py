d0a03c0a6700a960796f31abcd134e8aac973301
al.abir313@gmail.com <AbirAlFoysal>
2022-04-19 01:06:56 +0600

    full copy from askNans

--- Booksite/admin.py
+++ Booksite/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Post, Profile, Comment #, branch
 # Register your models here.
+admin.site.register(Post)
+# admin.site.register(branch)
+admin.site.register(Profile)
+admin.site.register(Comment)
--- Booksite/apps.py
+++ Booksite/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class BooksiteConfig(AppConfig):
-    name = 'Booksite'
+    name = 'booksite'
